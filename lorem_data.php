a1f151dd6da22e3434582297540f9a7903b90e6e
Michaelninder <fabianeternis@gmail.com>
2026-01-26 20:27:08 +0100

    feat: Add new /lorem endpoint

---
+++ lorem_data.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    '1' => [
+        'title' => 'Lorem Ipsum Dolor',
+        'content' => 'Consectetur adipiscing elit, sed do eiusmod tempor.',
+    ],
+    '2' => [
+        'title' => 'Sit Amet Consectetur',
+        'content' => 'Incididunt ut labore et dolore magna aliqua.',
+    ],
+    '3' => [
+        'title' => 'Adipiscing Elit Sed',
+        'content' => 'Ut enim ad minim veniam, quis nostrud exercitation.',
+    ],
+    '4' => [
+        'title' => 'Eiusmod Tempor Incididunt',
+        'content' => 'Ullamco laboris nisi ut aliquip ex ea commodo.',
+    ],
+    '5' => [
+        'title' => 'Labore Et Dolore',
+        'content' => 'Duis aute irure dolor in reprehenderit in voluptate.',
+    ],
+    '6' => [
+        'title' => 'Magna Aliqua Enim',
+        'content' => 'Velit esse cillum dolore eu fugiat nulla pariatur.',
+    ],
+    '7' => [
+        'title' => 'Minim Veniam Quis',
+        'content' => 'Excepteur sint occaecat cupidatat non proident.',
+    ],
+    '8' => [
+        'title' => 'Nostrud Exercitation Ullamco',
+        'content' => 'Sunt in culpa qui officia deserunt mollit anim.',
+    ],
+    '9' => [
+        'title' => 'Laboris Nisi Aliquip',
+        'content' => 'Est laborum sed ut perspiciatis unde omnis iste.',
+    ],
+    '10' => [
+        'title' => 'Commodo Consequat Duis',
+        'content' => 'Natus error sit voluptatem accusantium doloremque.',
+    ],
+    '11' => [
+        'title' => 'Irure Dolor Reprehenderit',
+        'content' => 'Laudantium, totam rem aperiam, eaque ipsa quae.',
+    ],
+    '12' => [
+        'title' => 'Voluptate Velit Esse',
+        'content' => 'Ab illo inventore veritatis et quasi architecto.',
+    ],
+    '13' => [
+        'title' => 'Cillum Dolore Fugiat',
+        'content' => 'Beatae vitae dicta sunt explicabo nemo enim.',
+    ],
+    '14' => [
+        'title' => 'Nulla Pariatur Excepteur',
+        'content' => 'Ipsam voluptatem quia voluptas sit aspernatur aut.',
+    ],
+    '15' => [
+        'title' => 'Occaecat Cupidatat Proident',
+        'content' => 'Odit aut fugit, sed quia consequuntur magni dolores.',
+    ],
+    '16' => [
+        'title' => 'Culpa Qui Officia',
+        'content' => 'Eos qui ratione voluptatem sequi nesciunt neque.',
+    ],
+    '17' => [
+        'title' => 'Deserunt Mollit Anim',
+        'content' => 'Porro quisquam est, qui dolorem ipsum quia dolor.',
+    ],
+    '18' => [
+        'title' => 'Laborum Perspiciatis Unde',
+        'content' => 'Sit amet, consectetur, adipisci velit, sed quia.',
+    ],
+    '19' => [
+        'title' => 'Omnis Iste Natus',
+        'content' => 'Non numquam eius modi tempora incidunt ut labore.',
+    ],
+    '20' => [
+        'title' => 'Voluptatem Accusantium Doloremque',
+        'content' => 'Magnam aliquam quaerat voluptatem ut enim ad.',
+    ],
+];
